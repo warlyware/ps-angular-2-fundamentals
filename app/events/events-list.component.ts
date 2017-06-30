9abb0b2d2020d8faf6e281a4d3d2bc539ee48aaa
Dan Ward <daniel.ward@forrent.com>
2017-06-30 10:47:34 -0400

    Separate event thumbnail from event list component

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -6,22 +6,12 @@ import { Component } from '@angular/core';
         <div>
             <h1>Upcoming Events</h1>
             <hr />
-            <div class="well hoverwell thumbnail">
-                <h2>{{event.name}}</h2>
-                <div>Date: {{event.date}}</div>
-                <div>Time: {{event.time}}</div>
-                <div>Price: {{event.price}}</div>
-                <div>
-                    <span>Location: {{event.location.address}}</span>
-                    <span>&nbsp;</span>
-                    <span>{{event.location.city}}, {{event.location.country}}</span>
-                </div>
-            </div>
+            <event-thumbnail [event]="event1"></event-thumbnail>
         </div>
     `
 })
 export class EventsListComponent {
-    public event = {
+    public event1 = {
         id: 1,
         name: 'Stupidcon',
         date: '5/3/18',
