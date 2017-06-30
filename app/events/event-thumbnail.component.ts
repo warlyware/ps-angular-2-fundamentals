224a94453bb51bd9dfd0fb839c763561e0e1af6d
Dan Ward <daniel.ward@forrent.com>
2017-06-30 14:38:29 -0400

    Use @Input, @Output, EventEmitter

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -1,23 +1,22 @@
-import { Component, Input } from '@angular/core';
+import {
+    Component,
+    EventEmitter,
+    Input,
+    Output
+} from '@angular/core';
 
 @Component({
     selector: 'event-thumbnail',
-    template: `
-        <div class="well hoverwell thumbnail">
-            <h2>{{event.name}}</h2>
-            <div>Date: {{event.date}}</div>
-            <div>Time: {{event.time}}</div>
-            <div>Price: \${{event.price}}</div>
-            <div>
-                <span>Location: {{event.location.address}}</span>
-                <span>&nbsp;</span>
-                <span>{{event.location.city}}, {{event.location.country}}</span>
-            </div>
-        </div>
-    `
+    templateUrl: '/app/events/event-thumbnail.html'
 })
 export class EventThumbnailComponent {
 
     @Input() public event: any;
+    @Output() public eventClick = new EventEmitter()
+
+    handleClickMe() {
+        this.eventClick.emit('fooooooo');
+        console.log('clicked');
+    }
 
 }
--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
         <div>
             <h1>Upcoming Events</h1>
             <hr />
-            <event-thumbnail [event]="event1"></event-thumbnail>
+            <event-thumbnail [event]="event1" (eventClick)="handleEventClicked($event)"></event-thumbnail>
         </div>
     `
 })
@@ -23,5 +23,9 @@ export class EventsListComponent {
             city: 'Dullsville',
             country: 'Blah'
         }
+    }
+
+    public handleEventClicked($event) {
+        console.log(`event received: ${this.event1.name}`);
     }
 }
